7aba3a9015ab7e2f630025eca5d1b35b85ce58d0
JoseMarin <jmarinmalagon@gamil.com>
2022-03-09 15:28:10 +0100

    ex6

---
+++ code_ex/ex6.py
@@ -0,0 +1,23 @@
+# Autor: jose marin
+# version: 0.0.1
+# data: 09/03/2022
+
+# Ex.6 - Declara 2 variables numéricas (con el valor que desees), he indica cual es mayor de los dos.
+# Si son iguales indicarlo también.Cambia los valores iniciales para comprobar que funciona correctamente.
+
+def compara():
+    valorA = int(input('Introduce valor de A:'))
+    valorB = int(input('Introduce valor de B:'))
+
+    if valorA > valorB:
+        print('A es mayor que B')
+    elif valorB > valorA:
+        print('B es mayor que A')
+    else:
+        print('A y B son iguales')
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Autor: jose marin
 # version: 0.0.1
 # data: 09/03/2022
-
+from code_ex.ex6 import compara
 
 if __name__ == '__main__':
-    print('hola mundo')
+    compara()
